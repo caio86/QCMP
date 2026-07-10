d673906822924c5ebbece66d070bf6850b68f320
caio86 <caioluiz86@gmail.com>
2026-07-10 18:12:01 -0300

    allow custom queue rate from input

--- set_switches.sh
+++ set_switches.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+
+RATE_P3=$1
+RATE_P4=$2
+
+if [ -z "$RATE_P3" ] || [ -z "$RATE_P4" ]; then
+    echo "Error: no port rates defined"
+    echo "Usage: $0 <rate_port_3> <rate_port_4>"
+    exit 1
+fi
+
 python3 initiate_rules.py
 
 simple_switch_CLI --thrift-port 9090 <<< 'set_queue_depth 200'
-simple_switch_CLI --thrift-port 9090 <<< 'set_queue_rate 50 3'
-simple_switch_CLI --thrift-port 9090 <<< 'set_queue_rate 50 4'
+simple_switch_CLI --thrift-port 9090 <<< "set_queue_rate $RATE_P3 3"
+simple_switch_CLI --thrift-port 9090 <<< "set_queue_rate $RATE_P3 4"
+
+# simple_switch_CLI --thrift-port 9090 <<< 'set_queue_depth 200'
+# simple_switch_CLI --thrift-port 9090 <<< 'set_queue_rate 200 3'
+# simple_switch_CLI --thrift-port 9090 <<< 'set_queue_rate 200 4'
 
 # simple_switch_CLI --thrift-port 9091 <<< 'set_queue_depth 200'
 # simple_switch_CLI --thrift-port 9091 <<< 'set_queue_rate 293 3'
