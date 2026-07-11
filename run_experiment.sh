0d28dc84ec7ca90a8a53d5547e8ce9da8bc92ec6
caio86 <caioluiz86@gmail.com>
2026-07-11 18:08:13 -0300

    automate h3 monitor on run_experiment.sh

--- run_experiment.sh
+++ run_experiment.sh
@@ -5,7 +5,7 @@ echo "Using configuration:"
 echo " -> Port 3 Queue Rate: $RATE_P3 pps"
 echo " -> Port 4 Queue Rate: $RATE_P4 pps"
 
-mkdir logs
+mkdir logs 2>/dev/null
 
 MININET_FIFO="./logs/mininet_input.fifo"
 rm -f $MININET_FIFO
@@ -27,6 +27,11 @@ echo "Initiating Telemetry on host 2"
 echo "h2 python3 get_queues_layer1.py > logs/h2_runtime.log 2> logs/h2_runtime.err &" > "$MININET_FIFO"
 
 sleep 3
+
+echo "Executing traffic monitor on host 3"
+echo "h3 python3 monitor_h3.py > logs/h3_monitor.log 2> logs/h3_monitor.err &" > "$MININET_FIFO"
+
+sleep 1
 
 echo "Begin main payload on host 1"
 echo "h1 python3 send.py > logs/h1_runtime.log 2> logs/h1_runtime.err &" > "$MININET_FIFO"
